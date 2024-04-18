2e555b146fb54763b9cbf7eeeab37e43c0b7c6bd
[Jin] <hahn23@my.bcit.ca>
2024-04-18 15:32:33 -0700

    first commit

---
+++ databaseAccessLayer.js
@@ -0,0 +1,104 @@
+const database = include('/databaseConnection');
+
+
+async function getAllItem() {
+	let sqlQuery = `
+	SELECT 
+    purchase_item_id,
+    item_name,
+    item_description,
+    FORMAT(cost, 4) AS cost,
+    quantity
+    FROM purchase_item;
+
+	`;
+	
+	try {
+		const results = await database.query(sqlQuery);
+        //console.log(" re : ")
+		//console.log(results[0]);
+		return results[0];
+	}
+	catch (err) {
+		console.log("Error selecting from puchase_item table");
+		console.log(err);
+		return null;
+	}
+}
+
+
+const addItem = async (postData) => {
+    console.log("postData: ", postData);
+
+    let sqlInsertItem = `
+        INSERT INTO purchase_item (item_name, item_description, cost, quantity)
+        VALUES (?, ?, ?, ?);
+    `;
+    
+    let params = [postData.item_name, postData.item_description, postData.cost, postData.quantity];
+
+
+    try {
+        const results = await database.query(sqlInsertItem, params);
+        console.log('Inserted item with ID:', results.insertId);
+        return results;
+    } catch (err) {
+        console.error("Error inserting into purchase_item table", err);
+        throw err; 
+    }
+};
+
+const deleteItem = async (itemId) => {
+    let sqlDeleteItem = `
+        DELETE FROM purchase_item WHERE purchase_item_id = ?;
+    `;
+
+    try {
+        const results = await database.query(sqlDeleteItem, [itemId]);
+        return results.affectedRows > 0; 
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+};
+
+const increaseItemQuantity = async (itemId) => {
+    let sqlUpdateQuery = `
+        UPDATE purchase_item
+        SET quantity = quantity + 1
+        WHERE purchase_item_id = ?;
+    `;
+
+    try {
+        const results = await database.query(sqlUpdateQuery, [itemId]);
+        console.log('Increased quantity of item with ID:', itemId);
+        return results;
+    } catch (err) {
+        console.error("Error increasing item quantity in MySQL", err);
+        throw err;
+    }
+};
+
+const decreaseItemQuantity = async (itemId) => {
+    let sqlUpdateQuery = `
+        UPDATE purchase_item
+        SET quantity = quantity - 1
+        WHERE purchase_item_id = ?;
+    `;
+
+    try {
+        const results = await database.query(sqlUpdateQuery, [itemId]);
+        console.log('decreased quantity of item with ID:', itemId);
+        return results;
+    } catch (err) {
+        console.error("Error increasing item quantity in MySQL", err);
+        throw err;
+    }
+};
+
+
+
+
+
+
+module.exports = {getAllItem, addItem, deleteItem, increaseItemQuantity, decreaseItemQuantity}
---
+++ routes/router.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('express').Router();
+const database = include('databaseConnection');
+const dbModel = include('databaseAccessLayer');
+//const dbModel = include('staticData');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.get('/', async (req, res) => {
+	console.log("page hit");
+	
+	try {
+		const result = await dbModel.getAllItem();
+		//console.log(`result : `)
+		//console.log(result);
+		res.render('index', {allItem: result});
+
+		//Output the results of the query to the Heroku Logs
+		console.log(result);
+	}
+	catch (err) {
+		res.render('error', {message: 'Error reading from MySQL'});
+		console.log("Error reading from mysql");
+	}
+});
+
+router.post('/addItem', async (req, res) => {
+    console.log("POST hit on /addItem");
+    
+    try {
+        const postData = {
+            item_name: req.body.name,
+            item_description: req.body.description,
+            cost: req.body.cost, 
+            quantity: req.body.quantity
+        };
+        // console.log("post data : ")
+        // console.log(postData)
+        const addResult = await dbModel.addItem(postData); 
+        console.log(`Add item result: `);
+        console.log(addResult);
+
+        if (addResult) {
+            res.redirect('/'); 
+        } else {
+            throw new Error('Failed to add new Item');
+        }
+    }
+    catch (err) {
+        console.error("Error adding new Item to MySQL", err);
+        res.render('error', { message: 'Error adding new Item to MySQL' });
+    }
+});
+
+router.get('/deleteItem', async (req, res) => {
+    const itemId = req.query.id;
+    if (!itemId) {
+        return res.status(400).send('item ID is required');
+    }
+
+    try {
+        const deleteResult = await dbModel.deleteItem(itemId);
+        if (deleteResult) {
+            console.log(`Deleted Item with ID: ${itemId}`);
+        } else {
+            console.log(`Item with ID: ${itemId} not found or could not be deleted`);
+        }
+        res.redirect('/'); 
+    } catch (err) {
+        console.error("Error deleting Item from MySQL", err);
+        res.status(500).send("Error deleting Item");
+    }
+});
+
+
+router.get('/increase', async (req, res) => {
+    console.log("GET: incrase")
+    const itemId = req.query.id;
+    console.log(`itemId : ${itemId}`);
+    if (!itemId) {
+        res.render('error', { message: 'missing item id' });
+    }
+
+    try {
+        const results = await dbModel.increaseItemQuantity(itemId);
+		console.log(results);
+        res.redirect('/');
+    } catch (err) {
+        console.error("Error increase item from MySQL", err);
+        res.render('error', { message: 'Error increase item' });
+    }
+});
+
+router.get('/decrease', async (req, res) => {
+    console.log("GET: decrease")
+    const itemId = req.query.id;
+    console.log(`itemId : ${itemId}`);
+    if (!itemId) {
+        res.render('error', { message: 'missing item id' });
+    }
+
+    try {
+        const results = await dbModel.decreaseItemQuantity(itemId);
+		console.log(results);
+        res.redirect('/');
+    } catch (err) {
+        console.error("Error decrease item from MySQL", err);
+        res.render('error', { message: 'Error decrease item' });
+    }
+});
+
+module.exports = router;
